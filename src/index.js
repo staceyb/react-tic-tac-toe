2dce5e0e67a74f1413ff0737cca97059a5aa755e
stacey <info@sunlitstud.io>
2018-04-12 18:04:33 -0700

    Added restart game button, cleaned up code - removed console.logs and unnecessary CSS

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class Board extends React.Component {
  
   renderSquare(i) {
     return (
-      <Square
+      <Square key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -50,11 +50,18 @@ class Game extends React.Component {
     };
   }
 
+  restartGame() {
+     let newState = { history: [{ squares: Array(9).fill(null), row: null, col: null }], 
+                 xIsNext: true, 
+                stepNumber: 0 };
+     this.setState(newState);
+  }
+
   handleClick(i) {
     const history = this.state.history.slice(0,this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    let isThereAWinner = this.calculateWinner(squares);
+    let isThereAWinner = this.calculateWinner(squares).winner;
   
     if (isThereAWinner || squares[i]) {
       return;
@@ -62,7 +69,7 @@ class Game extends React.Component {
    
     let row = Math.floor(i / 3) + 1;
     let col = (i % 3) + 1;
-    console.log(i, i % 3);
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
     this.setState({
@@ -81,10 +88,9 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = this.calculateWinner(current.squares);
+    const winningInfo = this.calculateWinner(current.squares);
 
     const moves = history.map((step, moveNum) => {
-      console.log({ step }, { moveNum });
       const desc = moveNum ? `Go to move #${moveNum} Row:${step.row} Col:${step.col}` : 'Go to game start';
       const currentStepClass = moveNum === this.state.stepNumber ? 'currentStep btn-underline' : 'btn-underline';
       return <li key={moveNum}>
@@ -95,24 +101,26 @@ class Game extends React.Component {
     });
 
     let status;
+    let winner = winningInfo.winner;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = 'Winner: ' + winningInfo.winner;
     } else if (!winner && this.state.history.length === 10) {
       status = 'The game is a draw: there is no winner';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-    return (
-      <div className="game">
+    return <div className="game">
         <div className="game-board">
           <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
+          <button className="restart" onClick={() => this.restartGame()}>
+            Restart Game
+          </button>
           <span className="status">{status}</span>
           <ol>{moves}</ol>
         </div>
-      </div>
-    );
+      </div>;
   }
 
   jumpTo(step) {
@@ -140,10 +148,13 @@ class Game extends React.Component {
         squares[a] === squares[b] &&
         squares[a] === squares[c]
       ) {
-        return squares[a];
+        return {
+                winningmoves: winningmoves[i],
+                winner: squares[a]
+              };
       }
     }
-    return null;
+    return {winningmoves: null, winner: null};
   }
 }
 
